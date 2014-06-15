256f98c38d43367d1768101cc8bd29f5c3f586af
WONG Tsz Yeung <tszyeung@gmail.com>
2014-06-15 17:19:45 +0800

    Adding more projects' information

--- js/app.js
+++ js/app.js
@@ -9,37 +9,9 @@
 		};
 		$http.get("js/projectInfo.js").success( function(data) {
 			info.projects = data;
+			info.projects.sort(function(a,b) {
+				return a.name.localeCompare(b.name);
+			});
 		});
 	}]);
-
-	// app.controller('projectInfoController', function($scope, $sce) {
-	// 	this.projects = data;
-	// 	// console.log(data);
-	// 	$scope.trustSrc = function(src) {
-	// 		return $sce.trustAsResourceUrl(src);
-	// 	};
-	// 	this.projects.sort(function(a,b) {
-	// 		return a.name.localeCompare(b.name);
-	// 	});
-	// });
-
-	// var data = [
-	// 	{
-	// 		name:  "Code Defense",
-	// 		youtubeURL: "https://www.youtube.com/embed/SayvtqIVGRc",
-	// 		repos: [
-	// 			{ description: "Github repo", url: "https://github.com/cuhk-csci4140/codeDefense" }
-	// 		],
-	// 		description: "A tower-defense type of game which uses JavaScript codes as the only way to control to the game character to move, attack, and win the game!"
-	// 	},
-	// 	{
-	// 		name: "N-Mario",
-	// 		youtubeURL: "https://www.youtube.com/embed/cPkCh6Z9aQ4",
-	// 		repos: [
-	// 			{ description: "The client page", url: "https://github.com/cuhk-csci4140/N-Mario" }, 
-	// 			{ description: "The Node JS server", url: "https://github.com/cuhk-csci4140/Node-Mario"}
-	// 		],
-	// 		description: "Not just a Mario clone. The game has introduced the awesome ingredients: multi-players on the same scene, cooperating to capture the flag, and, last but not least, betrayal!"
-	// 	}
-	// ];
 })();
--- js/projectInfo.js
+++ js/projectInfo.js
@@ -15,5 +15,30 @@
 			{ "description": "The Node JS server", "url": "https://github.com/cuhk-csci4140/Node-Mario"}
 		],
 		"description": "Not just a Mario clone. The game has introduced the awesome ingredients: multi-players on the same scene, cooperating to capture the flag, and, last but not least, betrayal!"
+	},
+	{
+		"name": "Peanut Wars",
+		"youtubeURL": "https://www.youtube.com/embed/TmwcrhMMdMg",
+		"repos": [
+			{"description": "Github repo", "url": "https://github.com/cuhk-csci4140/pWar"}
+		],
+		"description": "This is a strategy game - nothing related to peanuts ;) The UI and effects are awesome."
+	},
+	{
+		"name": "Footbomb",
+		"youtubeURL": "https://www.youtube.com/embed/Jwn2j5Kd9_E",
+		"live": "http://ec2-54-254-162-181.ap-southeast-1.compute.amazonaws.com/",
+		"description": "This is an interesting football game - the ball is a bomb!",
+		"repos": [
+			{"description": "Github repo", "url": "https://github.com/cuhk-csci4140/footbomb"}
+		]
+	},
+	{
+		"name": "PlaneWar",
+		"youtubeURL": "https://www.youtube.com/embed/V9Gljy4q3wk",
+		"description": "This is an online game using mobile devices, through the orientation detection by the accelerometer, as the game controller. <br><br>Look carefully: a player is using MacBook Pro as his controller!",
+		"repos": [
+			{"description": "Github repo", "url": "https://github.com/cuhk-csci4140/planeWar"}
+		]
 	}
 ]
